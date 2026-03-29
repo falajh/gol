8f78a96017a3c255833ac7693fbefcb9485328fb
falajh <almogtbanmr123@gmail.com>
2026-03-29 19:21:39 +0300

    format

--- core/game.go
+++ core/game.go
@@ -2,7 +2,7 @@ package core
 
 type State [][]bool
 
-func (st State) InBounds(y, x int) bool  {
+func (st State) InBounds(y, x int) bool {
 	if x < 0 || x >= len(st[0]) || y < 0 || y >= len(st) {
 		return false
 	}
@@ -28,24 +28,24 @@ func CreateState(rows, cols uint) State {
 }
 
 var directions = [][]int{
-	{0,1},
-	{1,1},
-	{1,0},
-	{1,-1},
-	{0,-1},
-	{-1,-1},
-	{-1,0},
-	{-1,1},
+	{0, 1},
+	{1, 1},
+	{1, 0},
+	{1, -1},
+	{0, -1},
+	{-1, -1},
+	{-1, 0},
+	{-1, 1},
 }
 
-//1 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-//2 Any live cell with two or three live neighbours lives on to the next generation.
-//3 Any live cell with more than three live neighbours dies, as if by overpopulation.
-//4 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+// 1 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+// 2 Any live cell with two or three live neighbours lives on to the next generation.
+// 3 Any live cell with more than three live neighbours dies, as if by overpopulation.
+// 4 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 func PlayRound(st State) State {
 	newState := CreateState(uint(len(st)), uint(len(st[0])))
 	for y, row := range st {
-		for x := range row{
+		for x := range row {
 			ncount := countNeigbours(st, x, y)
 			if ncount < 2 || ncount > 3 {
 				newState[y][x] = false
@@ -74,12 +74,11 @@ func PlayRoundsChan(st State) chan State {
 	return ch
 }
 
-
-func countNeigbours(st State, x, y int) (count int ) {
+func countNeigbours(st State, x, y int) (count int) {
 	for _, dir := range directions {
 		ny := y + dir[0]
 		nx := x + dir[1]
-		if !st.InBounds(ny, nx){
+		if !st.InBounds(ny, nx) {
 			continue
 		}
 		if st[ny][nx] {
